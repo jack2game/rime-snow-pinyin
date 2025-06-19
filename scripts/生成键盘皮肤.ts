5e52d30dce773d24b9c033b30d630ef14facc51d
Songchen Tan <i@tansongchen.com>
2025-06-19 15:57:03 -0700

    添加全键盘

--- scripts/生成键盘皮肤.ts
+++ scripts/生成键盘皮肤.ts
@@ -1,71 +1,191 @@
 import { dump } from "js-yaml";
 import { mkdirSync, writeFileSync } from "fs";
-import { join } from "path";
+import { join, parse } from "path";
 import { homedir } from "os";
 
 type Style = Record<string, any>;
 type Collection = Record<string, Style>;
 
 interface Config {
+  colorful: boolean;
   dark: boolean;
   label: boolean;
+  keyInfos: KeyInfo[][];
 }
 
 interface KeyInfo {
-  name: string;
+  action: string;
+  name?: string;
   label?: string;
   up?: string;
   upLabel?: string;
   down?: string;
+  downLabel?: string;
+  preedit?: string;
+  preeditLabel?: string;
+  width?: string;
 }
 
-const keyInfos: KeyInfo[][] = [
+const qpkr: KeyInfo[][] = [
   [
-    { name: "q", down: "~" },
-    { name: "p", down: "!" },
-    { name: "k", down: "@" },
-    { name: "r", down: "#" },
-    { name: "y", down: "$" },
-    { name: "t", down: "%" },
-    { name: "c", down: "_" },
+    { action: "q", down: "~" },
+    { action: "p", down: "!" },
+    { action: "k", down: "@" },
+    { action: "r", down: "#" },
+    { action: "y", down: "$" },
+    { action: "t", down: "%" },
+    { action: "c", down: "_" },
   ],
   [
-    { name: "j", down: "|" },
-    { name: "b", down: "^" },
-    { name: "g", down: "&" },
-    { name: "l", down: "*" },
-    { name: "w", down: "(" },
-    { name: "d", down: ")" },
-    { name: "z", down: "+" },
+    { action: "j", down: "|" },
+    { action: "b", down: "^" },
+    { action: "g", down: "&" },
+    { action: "l", down: "*" },
+    { action: "w", down: "(" },
+    { action: "d", down: ")" },
+    { action: "z", down: "+" },
   ],
   [
-    { name: "x", down: "`" },
-    { name: "lspace", up: "'", down: "1" },
-    { name: "h", down: "2" },
-    { name: "f", down: "3" },
-    { name: "v", down: "4" },
-    { name: "rspace", up: '"', down: "5" },
-    { name: "s", down: "-" },
+    { action: "x", down: "`" },
+    { action: "space", name: "lspace", up: "'", down: "1" },
+    { action: "h", down: "2" },
+    { action: "f", down: "3" },
+    { action: "v", down: "4" },
+    { action: "space", name: "rspace", up: '"', down: "5" },
+    { action: "s", down: "-" },
   ],
   [
-    { name: "m", down: "\\" },
-    { name: "o", down: "6" },
-    { name: "a", down: "7" },
-    { name: "i", down: "8" },
-    { name: "e", down: "9" },
-    { name: "u", down: "0" },
-    { name: "n", down: "=" },
+    { action: "m", down: "\\" },
+    { action: "o", down: "6" },
+    { action: "a", down: "7" },
+    { action: "i", down: "8" },
+    { action: "e", down: "9" },
+    { action: "u", down: "0" },
+    { action: "n", down: "=" },
   ],
   [
-    { name: "shift", label: "shift.fill", up: "#capsLocked", upLabel: "大写" },
-    { name: "semicolon", label: ";", up: ":", down: "[" },
-    { name: "comma", label: ",", up: "<", down: "{" },
-    { name: "backspace", label: "delete.left", up: "#重输", upLabel: "清空" },
-    { name: "period", label: ".", up: ">", down: "}" },
-    { name: "slash", label: "/", up: "?", down: "]" },
-    { name: "enter", label: "return", up: "#换行", upLabel: "换行" },
+    {
+      action: "#中英切换",
+      name: "ascii_mode",
+      label: "globe",
+      up: "shift",
+      upLabel: "上档",
+      preedit: "*Page_Down",
+      preeditLabel: "arrow.right.to.line",
+    },
+    { action: ";", name: "semicolon", up: ":", down: "[" },
+    { action: ",", name: "comma", up: "<", down: "{" },
+    { action: "backspace", label: "delete.left", up: "#重输", upLabel: "清空" },
+    { action: ".", name: "period", up: ">", down: "}" },
+    { action: "/", name: "slash", up: "?", down: "]" },
+    { action: "enter", label: "return", up: "#换行", upLabel: "换行" },
   ],
 ];
+
+const qwerty: KeyInfo[][] = [
+  [
+    { action: "1", name: "k1", up: "!", down: "!" },
+    { action: "2", name: "k2", up: "@", down: "@" },
+    { action: "3", name: "k3", up: "#", down: "#" },
+    { action: "4", name: "k4", up: "$", down: "$" },
+    { action: "5", name: "k5", up: "%", down: "%" },
+    { action: "6", name: "k6", up: "^", down: "^" },
+    { action: "7", name: "k7", up: "&", down: "&" },
+    { action: "8", name: "k8", up: "*", down: "*" },
+    { action: "9", name: "k9", up: "(", down: "(" },
+    { action: "0", name: "k0", up: ")", down: ")" },
+  ],
+  [
+    { action: "q", down: "_" },
+    { action: "w", down: "-" },
+    { action: "e", down: "+" },
+    { action: "r", down: "=" },
+    { action: "t", down: "|" },
+    { action: "y", down: "\\" },
+    { action: "u", down: "[" },
+    { action: "i", down: "]" },
+    { action: "o", down: "{" },
+    { action: "p", down: "}" },
+  ],
+  [
+    { action: "a", down: "~" },
+    { action: "s", down: "`" },
+    { action: "d", down: '"' },
+    { action: "f", down: "'" },
+    { action: "g", down: "#方案切换", downLabel: "方案" },
+    { action: "h", down: "*Left", downLabel: "←" },
+    { action: "j", down: "*Down", downLabel: "↓" },
+    { action: "k", down: "*Up", downLabel: "↑" },
+    { action: "l", down: "*Right", downLabel: "→" },
+    { action: ";", name: "semicolon", up: ":" },
+  ],
+  [
+    { action: "z", down: "#撤销", downLabel: "撤销" },
+    { action: "x", down: "#剪切", downLabel: "剪切" },
+    { action: "c", down: "#复制", downLabel: "复制" },
+    { action: "v", down: "#粘贴", downLabel: "粘贴" },
+    { action: "b", down: "#重做", downLabel: "重做" },
+    { action: "n", down: "*Home", downLabel: "行首" },
+    { action: "m", down: "*End", downLabel: "行尾" },
+    { action: ",", name: "comma", up: "<" },
+    { action: ".", name: "period", up: ">" },
+    { action: "/", name: "slash", up: "?" },
+  ],
+  [
+    {
+      action: "shift",
+      label: "shift.fill",
+      up: "#capsLocked",
+      upLabel: "大写",
+      preedit: "*Page_Down",
+      preeditLabel: "arrow.right.to.line",
+      width: "15/100",
+    },
+    {
+      action: "#中英切换",
+      name: "ascii_mode",
+      label: "globe",
+      up: "#简繁切换",
+      upLabel: "简繁",
+      preedit: "*Page_Up",
+      preeditLabel: "arrow.left.to.line",
+      width: "15/100",
+    },
+    {
+      action: "space",
+      label: "space",
+      up: "#RimeSwitcher",
+      upLabel: "选单",
+      width: "40/100",
+    },
+    {
+      action: "backspace",
+      label: "delete.left",
+      up: "#重输",
+      upLabel: "清空",
+      width: "15/100",
+    },
+    {
+      action: "enter",
+      label: "return",
+      up: "#换行",
+      upLabel: "换行",
+      width: "15/100",
+    },
+  ],
+];
+
+function parseAction(raw: string): Action {
+  if (raw.length === 1) {
+    return { character: raw };
+  } else if (raw.startsWith("#")) {
+    return { shortcutCommand: raw };
+  } else if (raw.startsWith("*")) {
+    return { sendKeys: raw.slice(1) };
+  } else {
+    return raw as Action;
+  }
+}
 
 function makeKeyboardLayoutStyles(config: Config): Collection {
   const baseColor = config.dark ? "707070" : "f7f7f7";
@@ -133,43 +253,36 @@ function makeKeyboardLayoutStyles(config: Config): Collection {
     };
     styles[`color${index}Alt`] = altBackgroundStyle;
   }
-  for (const row of keyInfos) {
+  for (const row of config.keyInfos) {
     for (const [keyIndex, key] of row.entries()) {
-      const isMain = key.name.length === 1 || key.label?.length === 1;
-      const isAlphabet = key.name.length === 1;
-      let action: Action;
-      if (isMain) {
-        action = { character: key.label ?? key.name };
-      } else {
-        action =
-          key.name === "lspace" || key.name === "rspace"
-            ? "space"
-            : (key.name as Action);
-      }
+      const isCharacter = key.action.length === 1;
+      const isAlphabet = /^[a-zA-Z]$/.test(key.action);
+      let name = key.name ?? key.action;
+      const action = parseAction(key.action);
       const foregroundStyle: string[] = [];
       const uppercasedStateForegroundStyle: string[] = [];
       const capsLockedStateForegroundStyle: string[] = [];
       if (action !== "space") {
         // label
-        const labelName = key.name + "Label";
+        const labelName = name + "Label";
         foregroundStyle.push(labelName);
-        if (isMain) {
+        if (isCharacter) {
           styles[labelName] = {
             ...structuredClone(mainLabel),
-            text: key.label ?? key.name,
+            text: key.label ?? key.action,
           };
         } else {
           styles[labelName] = {
             ...structuredClone(fnLabel),
-            systemImageName: key.label ?? key.name,
+            systemImageName: key.label ?? key.action,
           };
         }
       }
       let swipeUpAction: Action | undefined;
       let swipeDownAction: Action | undefined;
       if (key.up || isAlphabet) {
-        const up = key.up ?? key.name.toUpperCase();
-        const swipeUpName = key.name + "SwipeUp";
+        const up = key.up ?? key.action.toUpperCase();
+        const swipeUpName = name + "SwipeUp";
         if (!isAlphabet) foregroundStyle.push(swipeUpName);
         uppercasedStateForegroundStyle.push(swipeUpName);
         capsLockedStateForegroundStyle.push(swipeUpName);
@@ -177,32 +290,23 @@ function makeKeyboardLayoutStyles(config: Config): Collection {
           ...structuredClone(upSwipe),
           text: key.upLabel ?? up,
         };
-        swipeUpAction =
-          up.length === 1
-            ? {
-                character: up,
-              }
-            : {
-                shortcutCommand: up,
-              };
+        swipeUpAction = parseAction(up);
       }
       if (key.down) {
-        const swipeDownName = key.name + "SwipeDown";
+        const swipeDownName = name + "SwipeDown";
         foregroundStyle.push(swipeDownName);
         uppercasedStateForegroundStyle.push(swipeDownName);
         capsLockedStateForegroundStyle.push(swipeDownName);
         styles[swipeDownName] = {
           ...structuredClone(downSwipe),
-          text: key.down,
+          text: key.downLabel ?? key.down,
         };
-        swipeDownAction = {
-          character: key.down ?? key.name.toLowerCase(),
-        };
+        swipeDownAction = parseAction(key.down);
       }
       let backgroundStyle;
-      if (isMain) {
-        if (key.name.length === 1) {
-          backgroundStyle = "aeiou".includes(key.name)
+      if (isCharacter) {
+        if (isAlphabet && config.colorful) {
+          backgroundStyle = "aeiou".includes(key.action)
             ? `color${keyIndex}Alt`
             : `color${keyIndex}`;
         } else {
@@ -212,18 +316,18 @@ function makeKeyboardLayoutStyles(config: Config): Collection {
         backgroundStyle = "fnBackground";
       }
       const c: Cell = {
-        size: { width: "1/7" },
+        size: { width: key.width ?? `1/${row.length}` },
         backgroundStyle,
         foregroundStyle: config.label ? foregroundStyle : [],
         action,
         swipeUpAction,
         swipeDownAction,
       };
-      if (key.name.length === 1) {
-        const shiftName = key.name + "Shift";
+      if (isAlphabet) {
+        const shiftName = name + "Shift";
         styles[shiftName] = {
           ...structuredClone(mainLabel),
-          text: key.name.toUpperCase(),
+          text: key.action.toUpperCase(),
         };
         const foreground = [...c.foregroundStyle];
         foreground[0] = shiftName;
@@ -232,18 +336,19 @@ function makeKeyboardLayoutStyles(config: Config): Collection {
           ? structuredClone(foreground)
           : [];
         c.uppercasedStateAction = {
-          character: key.name.toUpperCase(),
+          character: key.action.toUpperCase(),
         };
       }
-      if (key.name === "shift") {
-        c.preeditStateAction = { sendKeys: "Tab" };
-        c.preeditStateForegroundStyle = "tab";
-        styles["tab"] = {
+      if (key.preedit) {
+        const preeditName = name + "Preedit";
+        c.preeditStateAction = parseAction(key.preedit);
+        c.preeditStateForegroundStyle = preeditName;
+        styles[preeditName] = {
           ...structuredClone(fnLabel),
-          systemImageName: "arrow.right.to.line",
+          systemImageName: key.preeditLabel ?? key.preedit,
         };
       }
-      styles[key.name] = c;
+      styles[name] = c;
     }
   }
   return styles;
@@ -277,7 +382,14 @@ function makeToolbar(config: Config): [Toolbar, Collection] {
   const toolbar: Toolbar = {
     backgroundStyle: "backgroundOriginalStyle",
     primaryButtonStyle: "hamster",
-    secondaryButtonStyle: ["dismiss", "ascii", "script", "clipboard", "phrase"],
+    secondaryButtonStyle: [
+      "dismiss",
+      "alphabetic",
+      "pinyin",
+      "script",
+      "clipboard",
+      "phrase",
+    ],
     horizontalCandidateStyle: "horizontalCandidate",
     verticalCandidateStyle: "verticalCandidate",
     candidateContextMenu: "candidateContextMenu",
@@ -305,20 +417,22 @@ function makeToolbar(config: Config): [Toolbar, Collection] {
     systemImageName: "chevron.down.circle",
     ...imageBase,
   };
-  const ascii: ToolbarButtonStyle = {
+  const alphabetic: ToolbarButtonStyle = {
     backgroundStyle: "buttonBackground",
-    foregroundStyle: `// JavaScript
-    function getText() {
-      return $getRimeOptionState("ascii_mode") ? "rimeImage" : "asciiImage";
-    }`,
-    action: { shortcutCommand: "#中英切换" },
+    foregroundStyle: "alphabeticImage",
+    action: { keyboardType: "alphabetic" },
   };
-  const asciiImage: Image = {
-    systemImageName: "character",
+  const alphabeticImage: Image = {
+    systemImageName: "characters.uppercase",
     ...imageBase,
   };
-  const rimeImage: Image = {
-    systemImageName: "characters.uppercase",
+  const pinyin: ToolbarButtonStyle = {
+    backgroundStyle: "buttonBackground",
+    foregroundStyle: "pinyinImage",
+    action: { keyboardType: "pinyin" },
+  };
+  const pinyinImage: Image = {
+    systemImageName: "character",
     ...imageBase,
   };
   const script: ToolbarButtonStyle = {
@@ -382,9 +496,10 @@ function makeToolbar(config: Config): [Toolbar, Collection] {
     hamsterImage,
     dismiss,
     dismissImage,
-    ascii,
-    asciiImage,
-    rimeImage,
+    alphabetic,
+    alphabeticImage,
+    pinyin,
+    pinyinImage,
     script,
     scriptImage,
     clipboard,
@@ -406,10 +521,9 @@ function makeKeyboardLayout(
   const keyboardStyle: KeyboardStyle = {
     backgroundStyle: "backgroundOriginalStyle",
   };
-  const keyboardLayout: KeyboardLayout = keyInfos.map((row) => {
-    const keys = row.map((key) => key.name);
+  const keyboardLayout: KeyboardLayout = config.keyInfos.map((row) => {
     return {
-      HStack: { subviews: keys.map((key) => ({ Cell: key })) },
+      HStack: { subviews: row.map((x) => ({ Cell: x.name ?? x.action })) },
     };
   });
   return [keyboardLayout, keyboardStyle, styles];
@@ -437,57 +551,70 @@ function makeHamsterSkin(config: Config): Keyboard {
 
 function makeHamsterSkinConfig(
   name: string,
-  pinyinKeyboard: string
+  keyboards: KeyboardCollection
 ): HamsterSkinConfig {
-  const pinyin: KeyboardConfig = {
+  const make = (name: string): KeyboardConfig => ({
     iPhone: {
-      portrait: pinyinKeyboard,
-      landscape: pinyinKeyboard,
+      portrait: name,
+      landscape: name,
     },
     iPad: {
-      portrait: pinyinKeyboard,
-      landscape: pinyinKeyboard,
-      floating: pinyinKeyboard,
+      portrait: name,
+      landscape: name,
+      floating: name,
     },
-  };
+  });
   return {
     name,
     author: "谭淞宸 <i@tansongchen.com>",
-    pinyin,
-    numeric: structuredClone(pinyin),
-    symbolic: structuredClone(pinyin),
+    pinyin: make("pinyin"),
+    alphabetic: keyboards.alphabetic ? make("alphabetic") : undefined,
+    numeric: keyboards.numeric ? make("numeric") : make("pinyin"),
+    symbolic: keyboards.symbolic ? make("symbolic") : make("pinyin"),
   };
 }
 
-function generateSkinFolder(
-  path: string,
-  skinName: string,
-  keyboardName: string,
-  label: boolean
-): void {
+type KeyboardType = "pinyin" | "alphabetic" | "numeric" | "symbolic";
+type KeyboardCollection = Partial<Record<KeyboardType, Omit<Config, "dark">>>;
+
+function generateSkinFolder(name: string, keyboards: KeyboardCollection) {
   const prefix = join(
     homedir(),
     "Library/Mobile Documents/iCloud~dev~fuxiao~app~hamsterapp/Documents/Skins/"
   );
-  const folder = join(prefix, path);
+  const folder = join(prefix, name);
   mkdirSync(folder, { recursive: true });
-  const skinConfig = makeHamsterSkinConfig(skinName, keyboardName);
+  const skinConfig = makeHamsterSkinConfig(name, keyboards);
   writeFileSync(join(folder, "config.yaml"), dump(skinConfig), "utf8");
-  const build = (config: Config) => {
-    const content = dump(makeHamsterSkin(config), { flowLevel: 2 });
-    const modeFolder = join(folder, config.dark ? "dark" : "light");
+  for (const dark of [false, true]) {
+    const modeFolder = join(folder, dark ? "dark" : "light");
     mkdirSync(modeFolder, { recursive: true });
-    const fileName = join(modeFolder, `${keyboardName}.yaml`);
-    writeFileSync(fileName, content, "utf8");
-  };
-  build({ dark: false, label });
-  build({ dark: true, label });
+    for (const [key, config] of Object.entries(keyboards)) {
+      const content = dump(makeHamsterSkin({ ...config, dark }), {
+        flowLevel: 2,
+      });
+      const fileName = join(modeFolder, `${key}.yaml`);
+      writeFileSync(fileName, content, "utf8");
+    }
+  }
 }
 
-generateSkinFolder("qpkr_rainbow", "俏皮可爱・七色", "qpkr_portrait", true);
-generateSkinFolder(
-  "qpkr_rainbow_nolabel",
-  "俏皮可爱・七色・无刻",
-  "qpkr_portrait",
-  false
-);
+const c1 = {
+  colorful: true,
+  label: true,
+  keyInfos: qpkr,
+};
+const c11 = {
+  colorful: true,
+  label: false,
+  keyInfos: qpkr,
+};
+const c2 = {
+  colorful: false,
+  label: true,
+  keyInfos: qwerty,
+};
+
+generateSkinFolder("俏皮可爱・七色", { pinyin: c1, alphabetic: c2 });
+generateSkinFolder("俏皮可爱・七色・无刻", { pinyin: c11, alphabetic: c2 });
+generateSkinFolder("直列 45 键", { pinyin: c2 });
